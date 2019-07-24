0d0271f96a5d20c06fd2b8eb3a4e1766c90bf6a1
pbosoo2 <52910474+pbosoo2@users.noreply.github.com>
2019-07-24 09:51:06 +0900

    최초 업로드

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.bxslider').bxSlider({
+        pager:false,
+        controls:false,
+        mode:'fade',
+        auto:true,
+        pause:5000,
+        oneToOneTouch: true
+    });
+});
